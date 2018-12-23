6256945420836446e5ee8e044626ecacaedebfa4
iloveooz <iloveooz@yandex.ru>
2018-12-23 12:19:49 +0700

    добавлены текстуры и спрайты

--- cars.cxx
+++ cars.cxx
@@ -3,7 +3,33 @@
 	
 	int main() {
 		
+		sf::RenderWindow app(sf::VideoMode(640, 480), "ROCK'N'ROLL RACING (not BLIZZARD)");
+		app.setFramerateLimit(60);
 		
+		sf::Texture tBackground, tCar;
+		tBackground.loadFromFile("images/background.png");
+		tCar.loadFromFile("images/car.png");
+		
+		sf::Sprite sBackground(tBackground), sCar(tCar);
+		sCar.setPosition(300, 300);
+		
+		while (app.isOpen()) {
+			sf::Event e;
+			
+			while (app.pollEvent(e)) {
+				if (e.type == sf::Event::Closed) 
+					app.close();
+			}
+			
+			// draw
+			app.clear(sf::Color::White);
+			app.draw(sBackground);
+			
+			sCar.setColor(sf::Color::Red);
+			app.draw(sCar);
+			
+			app.display();	
+		}
 		
 		return 0;
 	}
