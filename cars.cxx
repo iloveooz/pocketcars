50b68e407c95a7e00c9e291435fc7da81a3627de
iloveooz <iloveooz@yandex.ru>
2018-12-23 14:16:23 +0700

    написание движка закончено

--- cars.cxx
+++ cars.cxx
@@ -1,10 +1,60 @@
 	// небольшие покатушки на машинках
+	
+	// TODO сделать, чтобы машинки не выезжали за край карты
+	
 	#include <SFML/Graphics.hpp>
 	#include <cmath>
+
+	const int N = 5;
 	
+	// chackpoints
+	const int num = 8;
+	int points[num][2] = { 
+		300, 610,
+		1270, 430,
+		1380, 2380,
+		1900, 2460,
+		1970, 1700,
+		2550, 1680,
+		2560, 3150,
+		500, 3300
+	};
+	
+	struct Car {
+		float x, y, speed, angle;
+		int n;
+		
+		Car() {
+			speed = 2.0;
+			angle = 0.0;
+			n = 0;
+		}
+		
+		void move() {
+			x += sin(angle) * speed;
+			y -= cos(angle) * speed;
+			// angle += 0.08;
+			
+			float tx = points[n][0];
+			float ty = points[n][1];
+			
+			// плавный поворот
+			float beta = angle - atan2(tx - x, -ty + y);
+			if (sin(beta) < 0) 
+				angle += 0.08;
+			else
+				angle -= 0.08;
+			
+			if ((x - tx) * (x - tx) + (y - ty) * (y -ty) < 25 * 25)
+				n = (n + 1) % num;
+		}
+		
+	};
+
 	int main() {
 		
 		sf::RenderWindow app(sf::VideoMode(640, 480), "ROCK'N'ROLL RACING (not BLIZZARD)");
+		
 		app.setFramerateLimit(60);
 		
 		sf::Texture tBackground, tCar;
@@ -17,14 +67,21 @@
 		// центр всех преобразований фигуры
 		sCar.setOrigin(22, 22);
 		
-		float x = 300.0, y = 300.0;
+		Car car[N];
+		for (int i = 0; i < N; i++) {
+			car[i].x = 300 + i * 50;
+			car[i].y = 1700 + i * 80;
+			car[i].speed = 7 + i;
+		}
+		
+		
+		// float x = 300.0, y = 300.0;
 		float speed = 0.0, angle = 0.0;
 		float maxSpeed = 12.0;
 		float acc = 0.2, dec = 0.3;
 		float turnSpeed = 0.08;
 		
-		
-		
+		int offsetX = 0, offsetY = 0;
 		
 		
 		while (app.isOpen()) {
@@ -71,18 +128,54 @@
 			if (Left && speed != 0)
 				angle -= turnSpeed * speed / maxSpeed;
 			
-			x += sin(angle) * speed;
-			y -= cos(angle) * speed;
+			// x += sin(angle) * speed;
+			// y -= cos(angle) * speed;
+			
+			car[0].speed = speed;
+			car[0].angle = angle;
+			
+			for (int i = 0; i < N; i++) 
+				car[i].move();
+				
+			float R = 22;
+			
+			// столкновения
+			for (int i = 0; i < N; i++) 
+				for (int j = 0; j < N; j++) {
+					int dx = car[i].x - car[j].x;
+					int dy = car[i].y - car[j].y;
+					
+					if (dx * dx + dy * dy < 4 * R * R) {
+						car[i].x += dx / 10;
+						car[i].x += dy / 10;
+						car[j].x -= dx / 10;
+						car[j].y -= dy / 10;
+					}
+				}
 			
 			
+			if (car[0].x > 320) offsetX = car[0].x - 320;
+			if (car[0].y > 240) offsetY = car[0].y - 240;
+						
 			// draw
 			app.clear(sf::Color::White);
+			sBackground.setPosition(-offsetX, -offsetY);
 			app.draw(sBackground);
 			
-			sCar.setPosition(x, y);
-			sCar.setRotation(angle * 180 / 3.141592);
-			sCar.setColor(sf::Color::Red);
-			app.draw(sCar);
+			sf::Color colors[10] = {
+				sf::Color::Red,
+				sf::Color::Green, 
+				sf::Color::Magenta,
+				sf::Color::Blue,
+				sf::Color::White
+			};
+			
+			for (int i = 0; i < N; i++) {
+				sCar.setPosition(car[i].x - offsetX, car[i].y - offsetY);
+				sCar.setRotation(car[i].angle * 180 / 3.141592);
+				sCar.setColor(colors[i]);
+				app.draw(sCar);
+			}
 			
 			app.display();	
 		}
